7fd540040296ed2c9471a7f248d8a4634ea55084
Koonri <92974237+Koonri@users.noreply.github.com>
2022-03-23 14:35:57 +0100

    autocickbot

--- Clicker/Form1.Designer.cs
+++ Clicker/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.upgradebutton = new System.Windows.Forms.Button();
             this.buttonleveltextbox = new System.Windows.Forms.TextBox();
+            this.A1UpgradeAmount = new System.Windows.Forms.Button();
+            this.A1AmountTextbox = new System.Windows.Forms.TextBox();
+            this.A1UpgradeInterval = new System.Windows.Forms.Button();
+            this.A1Label = new System.Windows.Forms.Label();
+            this.A1IntervalTextBox = new System.Windows.Forms.TextBox();
+            this.A1timer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -55,9 +64,9 @@
             // 
             // upgradebutton
             // 
-            this.upgradebutton.Location = new System.Drawing.Point(453, 415);
+            this.upgradebutton.Location = new System.Drawing.Point(453, 385);
             this.upgradebutton.Name = "upgradebutton";
-            this.upgradebutton.Size = new System.Drawing.Size(75, 23);
+            this.upgradebutton.Size = new System.Drawing.Size(75, 53);
             this.upgradebutton.TabIndex = 2;
             this.upgradebutton.Text = "Upgrade";
             this.upgradebutton.UseVisualStyleBackColor = true;
@@ -73,11 +82,67 @@
             this.buttonleveltextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.buttonleveltextbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // A1UpgradeAmount
+            // 
+            this.A1UpgradeAmount.Location = new System.Drawing.Point(207, 5);
+            this.A1UpgradeAmount.Name = "A1UpgradeAmount";
+            this.A1UpgradeAmount.Size = new System.Drawing.Size(100, 54);
+            this.A1UpgradeAmount.TabIndex = 4;
+            this.A1UpgradeAmount.Text = "Ulepsz ilość";
+            this.A1UpgradeAmount.UseVisualStyleBackColor = true;
+            this.A1UpgradeAmount.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // A1AmountTextbox
+            // 
+            this.A1AmountTextbox.Location = new System.Drawing.Point(101, 22);
+            this.A1AmountTextbox.Name = "A1AmountTextbox";
+            this.A1AmountTextbox.Size = new System.Drawing.Size(100, 23);
+            this.A1AmountTextbox.TabIndex = 5;
+            this.A1AmountTextbox.Text = "0";
+            this.A1AmountTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // A1UpgradeInterval
+            // 
+            this.A1UpgradeInterval.Location = new System.Drawing.Point(419, 9);
+            this.A1UpgradeInterval.Name = "A1UpgradeInterval";
+            this.A1UpgradeInterval.Size = new System.Drawing.Size(97, 54);
+            this.A1UpgradeInterval.TabIndex = 6;
+            this.A1UpgradeInterval.Text = "Ulepsz częstotliwość";
+            this.A1UpgradeInterval.UseVisualStyleBackColor = true;
+            this.A1UpgradeInterval.Click += new System.EventHandler(this.A1UpgradeInterval_Click);
+            // 
+            // A1Label
+            // 
+            this.A1Label.AutoSize = true;
+            this.A1Label.Location = new System.Drawing.Point(12, 25);
+            this.A1Label.Name = "A1Label";
+            this.A1Label.Size = new System.Drawing.Size(83, 15);
+            this.A1Label.TabIndex = 7;
+            this.A1Label.Text = "Autoupgrade1";
+            // 
+            // A1IntervalTextBox
+            // 
+            this.A1IntervalTextBox.Location = new System.Drawing.Point(313, 22);
+            this.A1IntervalTextBox.Name = "A1IntervalTextBox";
+            this.A1IntervalTextBox.Size = new System.Drawing.Size(100, 23);
+            this.A1IntervalTextBox.TabIndex = 8;
+            this.A1IntervalTextBox.Text = "0";
+            this.A1IntervalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // A1timer
+            // 
+            this.A1timer.Tick += new System.EventHandler(this.A1tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(804, 453);
+            this.Controls.Add(this.A1IntervalTextBox);
+            this.Controls.Add(this.A1Label);
+            this.Controls.Add(this.A1UpgradeInterval);
+            this.Controls.Add(this.A1AmountTextbox);
+            this.Controls.Add(this.A1UpgradeAmount);
             this.Controls.Add(this.buttonleveltextbox);
             this.Controls.Add(this.upgradebutton);
             this.Controls.Add(this.button1);
@@ -96,5 +161,13 @@
         private Button button1;
         private Button upgradebutton;
         private TextBox buttonleveltextbox;
+        private Button A1UpgradeAmount;
+        private TextBox A1AmountTextbox;
+        private Button A1UpgradeInterval;
+        private Label A1Label;
+        private TextBox A1IntervalTextBox;
+        private System.Windows.Forms.Timer A1timer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
